302f05a6f619d2e9df46ad4bd90034518b2c1ae6
Sam Richard <snugug@google.com>
2020-01-09 15:48:33 -0600

    :wrench: Adding Commit Lint and Commitien

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@commitlint/config-lerna-scopes', 'gitmoji'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
+      headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
+    },
+  },
+};
