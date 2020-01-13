e13dd722e8ffbc5f11e06a48efffbe8f843c94de
Sam Richard <snugug@google.com>
2020-01-13 14:39:44 -0600

    🔧 (root) Use new Lerna Emoji for cz and commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -13,27 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const Project = require('@lerna/project');
 
-async function getPackages(context) {
-  const ctx = context || {};
-  const cwd = ctx.cwd || process.cwd();
-  const project = new Project(cwd);
-  const packages = (await project.getPackages()).map(pkg => pkg.name).map(name => (name.charAt(0) === '@' ? name.split('/')[1] : name));
-  packages.push('root');
-  return packages;
-}
+const config = require('./modules/lerna-emoji/commitlint');
 
-module.exports = {
-  extends: ['gitmoji'],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
-      headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
-    },
-  },
-  utils: { getPackages },
-  rules: {
-    'scope-enum': ctx => getPackages(ctx).then(packages => [2, 'always', packages]),
-  },
-};
+module.exports = config;
