88de3347fd75b72143725964bd7232cf4d2cbac2
Sam Richard <snugug@google.com>
2020-01-15 12:29:24 -0600

    🆕 (lerna-emoji) Add description to Lerna scopes generation

--- modules/lerna-emoji/lib/scopes.js
+++ modules/lerna-emoji/lib/scopes.js
@@ -1,11 +1,34 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const importCwd = require('import-cwd');
 const Project = importCwd('@lerna/project');
 
-module.exports = async function getPackages(context) {
+module.exports = async function getScopes(context) {
   const ctx = context || {};
   const cwd = ctx.cwd || process.cwd();
   const project = new Project(cwd);
-  const packages = (await project.getPackages()).map(pkg => pkg.name).map(name => (name.charAt(0) === '@' ? name.split('/')[1] : name));
-  packages.unshift('root');
+  const packages = (await project.getPackages()).map(pkg => ({
+    name: pkg.name.charAt(0) === '@' ? pkg.name.split('/')[1] : pkg.name,
+    description: pkg.get('description') || '',
+  }));
+
+  packages.unshift({
+    name: 'root',
+    description: 'Root project',
+  });
+
   return packages;
 };
