3a60fcda2d02bbba1c7191d05601b9236cc851f0
Sam Richard <snugug@google.com>
2020-03-24 16:57:08 -0500

    ♻️ (emoji-commit-helpers) Refactor config generation into a function

--- modules/emoji-commit-helpers/lib/config.js
+++ modules/emoji-commit-helpers/lib/config.js
@@ -2,42 +2,64 @@ const { cosmiconfigSync } = require('cosmiconfig');
 const emoji = require('./emoji');
 
 const path = require('path');
-const { existsSync } = require('fs');
+const fs = require('fs');
 
-const explorerSync = cosmiconfigSync('commit');
-const foundConfig = explorerSync.search();
-const userConfig = foundConfig ? foundConfig.config : {};
-let scopes = [];
-
-if (existsSync(path.join(process.cwd(), 'lerna.json'))) {
-  scopes = require('./scopes');
-} else {
-  scopes = userConfig.scopes;
-}
-
-const config = {
-  emoji,
-};
-
-if (typeof scopes === 'function') {
-  if (scopes.constructor.name !== 'AsyncFunction') {
-    config.scopes = async function getScopes() {
-      return scopes();
-    };
-  } else {
-    config.scopes = scopes;
-  }
-} else {
-  if (scopes.length === 0) {
-    throw new Error("Scopes can't be empty!");
-  }
-  config.scopes = async function getScopes() {
-    return scopes;
+/**
+ * Generates shared config
+ *
+ * @return {object} config object
+ */
+function generateConfig() {
+  const config = {
+    emoji,
   };
+
+  const explorerSync = cosmiconfigSync('commit');
+  const foundConfig = explorerSync.search();
+  const userConfig = foundConfig ? foundConfig.config : {};
+  let scopes = [];
+
+  if (exists(path.join(process.cwd(), 'lerna.json'))) {
+    scopes = require('./scopes');
+  } else {
+    scopes = userConfig.scopes || [];
+  }
+
+  if (typeof scopes === 'function') {
+    if (scopes.constructor.name !== 'AsyncFunction') {
+      config.scopes = async function getScopes() {
+        return scopes();
+      };
+    } else {
+      config.scopes = scopes;
+    }
+  } else {
+    if (scopes.length === 0) {
+      throw new Error("Scopes can't be empty!");
+    }
+    config.scopes = async function getScopes() {
+      return scopes;
+    };
+  }
+
+  if (userConfig.emoji) {
+    config.emoji = config.emoji.filter(emoji => userConfig.emoji.includes(emoji.name));
+  }
+
+  return config;
 }
 
-if (userConfig.emoji) {
-  config.emoji = config.emoji.filter(emoji => userConfig.emoji.includes(emoji.name));
+/**
+ * @param {string} filepath - Filepath to test
+ * @return {boolean}
+ */
+function exists(filepath) {
+  try {
+    (fs.accessSync || fs.statSync)(filepath);
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
 
-module.exports = config;
+module.exports = generateConfig();
