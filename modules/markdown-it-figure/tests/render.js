4cea0d06eb7363476616be30d591b9a3f73f1d14
Sam Richard <snugug@google.com>
2020-03-24 16:59:06 -0500

    👽 (markdown-it-figure) Update AVA syntax

--- modules/markdown-it-figure/tests/render.js
+++ modules/markdown-it-figure/tests/render.js
@@ -14,15 +14,29 @@
  * limitations under the License.
  */
 
-import test from 'ava';
-import markdown from 'markdown-it';
-import figurePlugin from '../index.js';
+const test = require('ava');
+const markdown = require('markdown-it');
+const figurePlugin = require('../index.js');
 
 const md = markdown({ html: true, linkify: true }).use(figurePlugin);
 
-test('ALl the bells and whistles', t => {
+test('All the bells and whistles', t => {
   const input = '#1234[Look At This Caption](/images/foo.png [Foo Bar])';
   const output = '<p><figure id="figure-1234"><img src="/images/foo.png" alt="Foo Bar"/><figcaption><p><span class="figure-id">1234: </span>Look At This Caption</p></figcaption></figure></p>\n';
+
+  t.is(md.render(input), output);
+});
+
+test('Weird Input', t => {
+  const input = '#asdf[Look At This Caption](/images/foo.png)';
+  const output = '<p>#asdf<a href="/images/foo.png">Look At This Caption</a></p>\n';
+
+  t.is(md.render(input), output);
+});
+
+test('No figure', t => {
+  const input = 'Look At This Caption';
+  const output = '<p>Look At This Caption</p>\n';
 
   t.is(md.render(input), output);
 });
