9e6b605563f77cb1f53ca6ca7b3fdc8cc353323b
Sam Richard <snugug@google.com>
2021-11-08 15:57:13 -0500

    🆕 (chromeos-dev-markdown) Add ChromeOS.dev Markdown
    Moves a number of modules out of ChromeOS.dev and Static Site Scaffold to make a reusable markdown
    renderer

---
+++ modules/chromeos-dev-markdown/index.js
@@ -0,0 +1,140 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const markdown = require('markdown-it');
+const { expand } = require('@emmetio/expand-abbreviation');
+const slugify = require('uslug');
+
+const Prism = require('prismjs/components/prism-core');
+const components = require('prismjs/components/index');
+
+// Load all components
+components();
+
+const md = markdown({
+  html: true,
+  linkify: true,
+  typographer: true,
+})
+  .use(require('markdown-it-deflist'))
+  .use(require('markdown-it-footnote'))
+  .use(require('markdown-it-sup'))
+  .use(require('markdown-it-abbr'))
+  .use(require('markdown-it-emoji'))
+  .use(require('markdown-it-attrs'))
+  .use(require('markdown-it-figure'))
+  .use(require('markdown-it-video'))
+  .use(require('markdown-it-header-sections'))
+  .use(require('markdown-it-kbd'))
+  .use(require('markdown-it-anchor'), {
+    slugify,
+    permalink: true,
+    renderPermalink(slug, opts, state, idx) {
+      const linkTokens = [
+        Object.assign(new state.Token('link_open', 'a', 1), {
+          attrs: [
+            ['class', 'header-anchor'],
+            ['href', `#${slug}`],
+          ],
+        }),
+        ...state.tokens[idx + 1].children,
+      ];
+
+      linkTokens.push(new state.Token('link_close', 'a', -1));
+
+      state.tokens[idx + 1].children = linkTokens;
+    },
+  })
+  .use(require('markdown-it-container'), 'emmet', {
+    marker: '!',
+    validate(params) {
+      return expand(params).split('</').length === 2;
+    },
+    render(tokens, idx) {
+      let token = tokens[idx];
+
+      if (token.nesting === 1) {
+        const expanded = expand(token.info);
+        const closing = expanded.lastIndexOf('</');
+        return expanded.substring(0, closing);
+      }
+      while (token.info === '') {
+        idx--;
+        token = tokens[idx];
+      }
+      const expanded = expand(token.info);
+      const closing = expanded.lastIndexOf('</');
+      return expanded.substring(closing, expanded.length);
+    },
+  })
+  .disable('code');
+
+// Blockquote rules
+// Optional cite moves to blockquote footer
+md.renderer.rules.blockquote_open = function(tokens, idx, options, env, slf) {
+  const token = tokens[idx];
+  const citeIndex = Array.isArray(token.attrs) ? token.attrs.findIndex(attr => attr[0] === 'cite') : null;
+
+  if (citeIndex !== null) {
+    const nextClose = tokens.find(t => t.type === 'blockquote_close');
+    nextClose.cite = token.attrs[citeIndex][1];
+    token.attrs.splice(citeIndex, 1);
+  }
+
+  const blockquoteTokenHTML = `<blockquote${slf.renderAttrs(token)}>`;
+
+  return blockquoteTokenHTML;
+};
+
+md.renderer.rules.blockquote_close = function(tokens, idx, options, env, slf) {
+  const token = tokens[idx];
+  const { cite } = token;
+
+  if (cite) {
+    return `<footer>${cite}</footer></blockquote>`;
+  }
+
+  return '</blockquote>';
+};
+
+// Code Fence rules
+// Wraps code fences in figures, optional titles become figcaptions
+md.renderer.rules.fence = function(tokens, idx, options, env, slf) {
+  const token = tokens[idx];
+  const titleIndex = Array.isArray(token.attrs) ? token.attrs.findIndex(attr => attr[0] === 'title') : null;
+  let title = null;
+
+  if (titleIndex !== null) {
+    title = token.attrs[titleIndex][1];
+    token.attrs.splice(titleIndex, 1);
+  }
+
+  let languageTokenHTML = `<figure${slf.renderAttrs(token)}>`;
+  if (title) {
+    languageTokenHTML += `<figcaption>${title}</figcaption>`;
+  }
+
+  if (token.info) {
+    languageTokenHTML += `<pre class="language-${token.info}"><code class="language-${token.info}">${Prism.highlight(token.content, Prism.languages[token.info], token.info)}</code></pre>`;
+  } else {
+    languageTokenHTML += `<pre class="language-unknown"><code class="language-unknown">${token.content}</code></pre>`;
+  }
+
+  languageTokenHTML += `</figure>`;
+
+  return languageTokenHTML;
+};
+
+module.exports = md;
---
+++ modules/chromeos-dev-markdown/tests/markdown.js
@@ -0,0 +1,66 @@
+const test = require('ava');
+const md = require('../index.js');
+
+test('Renders fences with titles', t => {
+  const content = `
+\`\`\`javascript {title="Hello World" .foo .bar #baz}
+const a = 1;
+\`\`\``;
+  const output = md.render(content);
+  const expected = `<figure class="foo bar" id="baz"><figcaption>Hello World</figcaption><pre class="language-javascript"><code class="language-javascript"><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre></figure>`;
+
+  t.is(output, expected);
+});
+
+test('Renders unknown languages', t => {
+  const content = `
+\`\`\`
+const a = 1;
+\`\`\``;
+  const output = md.render(content);
+  const expected = `<figure><pre class="language-unknown"><code class="language-unknown">const a = 1;
+</code></pre></figure>`;
+
+  t.is(output, expected);
+});
+
+test('Renders blockquotes with citations without carrying over to next blockquote', t => {
+  const content = `> This is such a quote.
+  >
+  > And some more of this quote
+  {.foo cite="Banana bob"}
+  
+  > Another Blockquote`;
+  const output = md.render(content);
+  const expected = `<blockquote class="foo"><p>This is such a quote.</p>
+<p>And some more of this quote</p>
+<footer>Banana bob</footer></blockquote><blockquote><p>Another Blockquote</p>
+</blockquote>`;
+
+  t.is(output, expected);
+});
+
+test('Renders headers with unicode, ids, and sections', t => {
+  const content = `# Hélløoö 🌎 Test
+This is some content`;
+  const output = md.render(content);
+  const expected = `<section>
+<h1 id="hélløoö-test" tabindex="-1"><a class="header-anchor" href="#hélløoö-test">Hélløoö 🌎 Test</a></h1>
+<p>This is some content</p>
+</section>
+`;
+
+  t.is(output, expected);
+});
+
+test('Emmet Containers', t => {
+  const content = `!!! aside.message.message--alert
+**Alert**: This is an alert message
+!!!`;
+  const output = md.render(content);
+  const expected = `<aside class="message message--alert"><p><strong>Alert</strong>: This is an alert message</p>
+</aside>`;
+
+  t.is(output, expected);
+});
